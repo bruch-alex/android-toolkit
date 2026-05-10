c6099fedd000f97d88dbe33f81df374e408a8f8b
Alex Bruch <alex.bruch@tutanota.com>
2026-05-10 12:36:08 +0200

    add menubar, add separate scene if device is not connected

--- src/main/java/app/androidtoolkit/App.java
+++ src/main/java/app/androidtoolkit/App.java
@@ -3,6 +3,7 @@ package app.androidtoolkit;
 import app.androidtoolkit.service.ADBService;
 import atlantafx.base.theme.PrimerDark;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -10,17 +11,38 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class App extends Application {
+    private final AppState appState = AppState.getInstance();
+    private Scene connectedDeviceStage;
+    private Scene setupStage;
+    private Stage primaryStage;
+
     @Override
     public void start(Stage stage) throws IOException {
+        this.primaryStage = stage;
         Application.setUserAgentStylesheet(new PrimerDark().getUserAgentStylesheet());
         ADBService adb = ADBService.getInstance();
         adb.start();
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("fxml/main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1080);
-        stage.setTitle("Android Device Toolkit");
-        stage.setScene(scene);
-        stage.setMinWidth(1280);
-        stage.setMinHeight(720);
-        stage.show();
+
+        FXMLLoader setupLoader = new FXMLLoader(App.class.getResource("fxml/setup-adb.fxml"));
+        setupStage = new Scene(setupLoader.load(), 1920, 1080);
+
+        FXMLLoader appLoader = new FXMLLoader(App.class.getResource("fxml/main-view.fxml"));
+        connectedDeviceStage = new Scene(appLoader.load(), 1920, 1080);
+
+        primaryStage.setTitle("Android Device Toolkit");
+        primaryStage.setScene(setupStage);
+        primaryStage.setMinWidth(1280);
+        primaryStage.setMinHeight(720);
+        primaryStage.show();
+
+        startMonitoring();
+    }
+
+    private void startMonitoring() {
+        appState.getConnectedDevice().addListener((_, _, newDevice) -> {
+            if (newDevice != null) {
+                Platform.runLater(() -> primaryStage.setScene(connectedDeviceStage));
+            } else Platform.runLater(() -> primaryStage.setScene(setupStage));
+        });
     }
 }
---
+++ src/main/java/app/androidtoolkit/controller/SetupScreenController.java
@@ -0,0 +1,5 @@
+package app.androidtoolkit.controller;
+
+public class SetupScreenController {
+
+}
