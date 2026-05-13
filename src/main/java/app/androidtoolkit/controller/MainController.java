1e9ff97dd4bb5a216b84d6737f32a251f4e661ff
Alex Bruch <alex.bruch@tutanota.com>
2026-05-13 12:41:28 +0200

    add logger

--- src/main/java/app/androidtoolkit/controller/MainController.java
+++ src/main/java/app/androidtoolkit/controller/MainController.java
@@ -1,10 +1,12 @@
 package app.androidtoolkit.controller;
 
 import javafx.event.ActionEvent;
+import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
 import java.net.URI;
 
+@Slf4j
 public class MainController {
     public void openSourceCode(ActionEvent actionEvent) {
         Thread thread = new Thread(() -> {
@@ -13,10 +15,9 @@ public class MainController {
                         new URI("https://github.com/bruch-alex/android-toolkit")
                 );
             } catch (Exception e) {
-                e.printStackTrace();
+                log.error("Failed to open source code", e);
             }
         });
-
         thread.setDaemon(true);
         thread.setName("browser-opener");
         thread.start();
