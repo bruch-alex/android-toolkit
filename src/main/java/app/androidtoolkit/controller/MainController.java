58724cf134a972361f1fa02e8cebf3a9c8714920
Alex Bruch <alex.bruch@tutanota.com>
2026-05-13 12:04:25 +0200

    add openSourceCode logic

--- src/main/java/app/androidtoolkit/controller/MainController.java
+++ src/main/java/app/androidtoolkit/controller/MainController.java
@@ -1,5 +1,25 @@
 package app.androidtoolkit.controller;
 
+import javafx.event.ActionEvent;
+
+import java.awt.*;
+import java.net.URI;
+
 public class MainController {
+    public void openSourceCode(ActionEvent actionEvent) {
+        Thread thread = new Thread(() -> {
+            try {
+                Desktop.getDesktop().browse(
+                        new URI("https://github.com/bruch-alex/android-toolkit")
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        thread.setDaemon(true);
+        thread.setName("browser-opener");
+        thread.start();
+    }
 }
 
