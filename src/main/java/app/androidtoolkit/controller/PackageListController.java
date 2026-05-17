c82f6767d88ef402e399eed2bc366a42c457fcb0
Alex Bruch <alex.bruch@tutanota.com>
2026-05-17 15:06:23 +0200

    refactor: move context menu creation logic into separate ContextMenuUtils class

--- src/main/java/app/androidtoolkit/controller/PackageListController.java
+++ src/main/java/app/androidtoolkit/controller/PackageListController.java
@@ -4,7 +4,7 @@ import app.androidtoolkit.AppState;
 import app.androidtoolkit.model.AndroidUser;
 import app.androidtoolkit.model.AppPackage;
 import app.androidtoolkit.service.ADBService;
-import app.androidtoolkit.utils.DialogUtils;
+import app.androidtoolkit.utils.ContextMenuUtils;
 import app.androidtoolkit.viewmodel.DeviceView;
 import javafx.animation.PauseTransition;
 import javafx.collections.FXCollections;
@@ -14,14 +14,8 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.scene.control.*;
-import javafx.scene.input.Clipboard;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.KeyCombination;
 import javafx.util.Duration;
 import lombok.extern.slf4j.Slf4j;
-import org.kordamp.ikonli.Ikon;
-import org.kordamp.ikonli.fontawesome6.FontAwesomeSolid;
-import org.kordamp.ikonli.javafx.FontIcon;
 
 import java.util.Comparator;
 
@@ -124,7 +118,7 @@ public class PackageListController {
                     setText(null);
                 } else {
                     setText(item.getPackageName());
-                    setContextMenu(createContextMenuForPackage(item));
+                    setContextMenu(ContextMenuUtils.createContextMenuForPackage(item));
                 }
             }
         });
@@ -188,68 +182,5 @@ public class PackageListController {
             var matchesDisabledFilter = !hideDisabledAppsCheckBox.isSelected() || app.getInstanceDetailsMap().get(appState.getSelectedUser().get().id()).isEnabled();
             return matchesSearch && matchesSystemFilter && matchesDisabledFilter;
         });
-    }
-
-    private ContextMenu createContextMenuForPackage(AppPackage appPackage) {
-        var contextMenu = new ContextMenu();
-        contextMenu.getItems().addAll(
-                createCopyMenuItem(),
-                createDeleteMenuItem(appPackage),
-                createDisableMenuItem(appPackage),
-                createItem("Share package details", FontAwesomeSolid.SHARE, null),
-                createItem("Export details to pdf", FontAwesomeSolid.FILE_PDF, null)
-        );
-        return contextMenu;
-    }
-
-    private MenuItem createDisableMenuItem(AppPackage appPackage) {
-        var menuItem = createItem("Disable package", FontAwesomeSolid.STOP, null);
-        menuItem.setOnAction(_ -> {
-            if (DialogUtils.showConfirmationDialog("Disable Package", "Are you sure you want to disable this package?")) {
-                try {
-                    adb.setEnabledToDisabled(appPackage.getPackageName(), selectedUserBox.getSelectionModel().getSelectedItem().id());
-                } catch (Exception e) {
-                    log.error("Failed to disable package: {}", appPackage.getPackageName(), e);
-                }
-            }
-        });
-        return menuItem;
-    }
-
-    private MenuItem createDeleteMenuItem(AppPackage appPackage) {
-        var menuItem = createItem("Delete package", FontAwesomeSolid.TRASH, null);
-        menuItem.setOnAction(_ -> {
-            if (DialogUtils.showConfirmationDialog("Delete Package", "Are you sure you want to delete this package?", true)) {
-                try {
-                    adb.deleteAppForUser(appPackage.getPackageName(), selectedUserBox.getSelectionModel().getSelectedItem().id());
-                } catch (Exception e) {
-                    log.error("Failed to delete package: {}", appPackage.getPackageName(), e);
-                }
-            }
-        });
-        return menuItem;
-    }
-
-    private MenuItem createCopyMenuItem() {
-        var menuItem = createItem("Copy package name", FontAwesomeSolid.COPY, null);
-        menuItem.setOnAction(_ -> {
-            var clipboard = Clipboard.getSystemClipboard();
-            var content = new ClipboardContent();
-            content.putString(appState.getSelectedPackage().get().getPackageName());
-            clipboard.setContent(content);
-        });
-        return menuItem;
-    }
-
-    private MenuItem createItem(String text, Ikon graphic, KeyCombination accelerator) {
-        var item = new MenuItem(text);
-        if (graphic != null) {
-            item.setGraphic(new FontIcon(graphic));
-        }
-
-        if (accelerator != null) {
-            item.setAccelerator(accelerator);
-        }
-        return item;
     }
 }
