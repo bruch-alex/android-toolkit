a67e1f312d4929e0ca999046dbc39d199e979b2a
Alex Bruch <alex.bruch@tutanota.com>
2026-05-18 15:57:50 +0200

    refactor: remove buttons from package details view

--- src/main/java/app/androidtoolkit/controller/PackageInfoController.java
+++ src/main/java/app/androidtoolkit/controller/PackageInfoController.java
@@ -19,8 +19,6 @@ public class PackageInfoController {
     public Label enabledStatusLabel;
     public Label appIdLabel;
     public Label versionNameLabel;
-    public Button toggleEnabledStatusButton;
-    public Button deletePackageButton;
     public ListView<String> queriedPackagesListView;
     public GridPane container;
 
@@ -42,51 +40,13 @@ public class PackageInfoController {
                     totalQueriedPackagesLabel.setText(String.valueOf(newPackage.getPackageDetails().getQueriesPackages().size()));
                     queriedPackagesListView.setItems(
                             FXCollections.observableArrayList(newPackage.getPackageDetails().getQueriesPackages()));
-                    toggleEnabledStatusButton.setText(enabled ? "Disable" : "Enable");
-
-                    toggleEnabledStatusButton.setOnAction(_ -> toggleEnabledStatus(newPackage, enabled));
-                    deletePackageButton.setOnAction(_ -> deletePackage(newPackage));
-
                     container.setVisible(true);
                 });
             }
         });
     }
 
-    private void toggleEnabledStatus(AppPackage newPackage, boolean enabled) {
-        var uid = appState.getSelectedUser().get().id();
-        var packageName = newPackage.getPackageName();
-        if (enabled) {
-            try {
-                if (DialogUtils.showConfirmationDialog("Disable Package", "Are you sure you want to disable this package?")) {
-                    adb.setEnabledToDisabled(packageName, uid);
-                }
 
-            } catch (Exception e) {
-                log.error("Failed to disable package: {}", packageName, e);
-            }
-        } else {
-            try {
-                if (DialogUtils.showConfirmationDialog("Enable Package", "Are you sure you want to enable this package?")) {
-                    adb.setEnabledToDefaultState(packageName, uid);
-                }
-            } catch (Exception e) {
-                log.error("Failed to enable package: {}", packageName, e);
-            }
-        }
-        appState.forceUpdateSelectedPackage();
-    }
 
-    private void deletePackage(AppPackage newPackage) {
-        var uid = appState.getSelectedUser().get().id();
-        var packageName = newPackage.getPackageName();
-        try {
-            if (DialogUtils.showConfirmationDialog("Delete Package", "Are you sure you want to delete this package?", true)) {
-                adb.deleteAppForUser(packageName, uid);
-            }
-        } catch (Exception e) {
-            log.error("Failed to delete package: {}", packageName, e);
-        }
-    }
 }
 
